569494f548ef831fa7c3f3a1dda364c13c19dfa7
Glitch (cn-lesson-template) <none>
2020-02-22 15:06:45 +0000

    🏢👘 Checkpoint ./index.html:671814/250 ./lessonData.js:671814/1274 ./students.html:671814/116

--- lessonData.js
+++ lessonData.js
@@ -4,5 +4,10 @@ const lessonData = {
     DN: "https://popcode.org/?snapshot=9ca51617-da4e-4e43-baea-22e799857ab8",
     GP: "https://glitch.com/edit/#!/p5js-startercode?path=README.md:1:0",
     IP: "https://www.w3schools.com/"
-  }
+  },
+  multiplex: {
+    secret: '15823830584203231343',
+    id: 'b7f68999921608a1',
+    url: 'https://reveal-js-multiplex-ccjbegmaii.now.sh'
+  },
 };
